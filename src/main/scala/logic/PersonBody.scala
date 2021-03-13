6eedccc01689d985fee94fad11ef89fceae4bd2a
Yura <george.ananov@gmail.com>
2021-03-13 22:07:43 +0200

    Implemented basic "brainless" functionality for Room and PersonBody

--- src/main/scala/logic/PersonBody.scala
+++ src/main/scala/logic/PersonBody.scala
@@ -1,7 +1,23 @@
 package logic
 
-class PersonBody {
+class PersonBody(var location: Vector2d) {
 
  private var brain: Option[PersonBrain] = None
+ private var currentVelocity: Vector2d = Vector2d(0.0, 0.0)
 
+ def updateVelocity() = {
+  brain match {
+   case Some(brain) =>
+      currentVelocity = currentVelocity + (brain.targetVelocity() - currentVelocity).capMagnitude(PersonBody.MAX_ACC)
+   case None =>
+  }
+ }
+
+ def move(timePassed: Double) = location += currentVelocity * (PersonBody.MAX_SPD * timePassed)
+
+}
+
+object PersonBody {
+  private val MAX_ACC: Double = 0.1
+  private val MAX_SPD: Double = 1.0
 }
--- src/main/scala/logic/Room.scala
+++ src/main/scala/logic/Room.scala
@@ -2,5 +2,11 @@ package logic
 
 class Room(val people: Vector[PersonBody]) {
 
+  def coordinateList = people.map( _.location )
+
+  def step(timePassed: Double) = {
+    people.foreach( _.updateVelocity() )
+    people.foreach( _.move(timePassed) )
+  }
 
 }

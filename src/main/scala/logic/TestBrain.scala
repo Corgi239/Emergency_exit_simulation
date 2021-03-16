ff81dfbde3ad170f2dc1dcb5f8caa897644512ac
Yura <george.ananov@gmail.com>
2021-03-16 20:09:40 +0200

    Created and updated the progress_log file

--- src/main/scala/logic/TestBrain.scala
+++ src/main/scala/logic/TestBrain.scala
@@ -2,5 +2,5 @@ package logic
 
 class TestBrain(body: PersonBody) extends PersonBrain(body){
 
-  override def targetVelocity(): Vector2d = Vector2d(-1, -0.1)
+  override def targetVelocity(): Vector2d = Vector2d(1, -1)
 }

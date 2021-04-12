618400e34531b5d275ac000d6675e65d218aa9aa
Yura <george.ananov@gmail.com>
2021-04-12 23:37:59 +0300

    Improved the containment steering behaviour

--- src/main/scala/logic/PersonBody.scala
+++ src/main/scala/logic/PersonBody.scala
@@ -8,7 +8,7 @@ class PersonBody(var location: Vector2d, room: Room) {
   private var maxAcc: Double = 0.0001
   private var maxSpd: Double = 0.05
   private var searchRadius = 25.0
-  private val containmentProbeDistance = 20.0
+  private val containmentProbeDistance = 5.0
 
   def maxSpeed = maxSpd
   def setMaxSpeed(updMaxSpd: Double) = maxSpd = updMaxSpd
@@ -16,8 +16,10 @@ class PersonBody(var location: Vector2d, room: Room) {
   def getExitMiddle = room.exitMiddle
   def getNeighbors = room.neighbors(this.location, searchRadius)
   def getContainmentNormal: Vector2d = {
-    val probe = this.location + (currentVelocity.normalize() * containmentProbeDistance)
-    room.getBoundaryNormal(probe)
+    val probe1 = this.location + (currentVelocity.normalize() * containmentProbeDistance)
+    val probe2 = this.location + ((currentVelocity.clockwise() + currentVelocity).normalize() * (containmentProbeDistance * 2))
+    val probe3 = this.location + ((currentVelocity.counterclockwise() + currentVelocity).normalize() * (containmentProbeDistance * 2))
+    room.getBoundaryNormal(probe1) + room.getBoundaryNormal(probe2) + room.getBoundaryNormal(probe3)
   }
 
   def giveBrain(brain: PersonBrain) = this.brain = Some(brain)
